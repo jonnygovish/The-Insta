77ea3a113e221c6faad67ab4644cefac2e1eef25
John <jonnygovish@gmail.com>
2017-11-27 08:34:00 +0300

    CReating routes

--- gram/urls.py
+++ gram/urls.py
@@ -6,4 +6,9 @@ from .import views
 
 urlpatterns =[
   url(r'^$', views.index, name='Home'),
+  url(r'^profile/(?P<username>[-_\w.]+)/$', views.profile, name='profile'),
+  url(r'^profile/(?P<username>[-_\w.]+)/edit/$', views.update_profile, name='edit'),
+  url(r'^profile/(?P<username>[-_\w.]+)/followers/$', views.followers, name='followers'),
+  url(r'^profile/(?P<username>[-_\w.]+)/following/$', views.following, name='following'),
+  url(r'^post/$', views.posts, name='post_picture'),
 ]
