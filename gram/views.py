5fe0d7658cd485d374a783c0e5306a75674833f2
John <jonnygovish@gmail.com>
2017-12-06 18:09:11 +0300

    View function for displaying post for users who are following you

--- gram/views.py
+++ gram/views.py
@@ -6,12 +6,16 @@ from django.db import transaction
 from django.contrib.auth.models import User
 from .models import Profile,Post,Comment,Like
 from django.core.urlresolvers import reverse
+from django.http import JsonResponse
+from annoying.decorators import ajax_request
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
   # Only return posts from users that are being followed
-  posts = Post.objects.filter().all().order_by('-id')
+  users_followed = request.user.profile.following.all()
+  posts = Post.objects.filter(profile__in=users_followed).order_by('-posted_on')
+
 
   return render(request,'index.html',{"posts":posts})
 
@@ -26,7 +30,7 @@ def update_profile(request, username):
       user_form.save()
       profile_form.save()
       messages.success(request, ('Your profile was successfully updated!'))
-      return redirect(reverse('profile', kwargs={'username': user.username}))
+      return redirect(reverse('profile', kwargs={'username': request.user.username}))
     else:
       messages.error(request, ('Please correct the error below.'))
   else:
@@ -58,7 +62,7 @@ def followers(request, username):
 def following(request, username):
   user = user = User.objects.get(username = username)
   user_profile = Profile.objects.get(user=user)
-  profiles = user_profile.following.all
+  profiles = user_profile.following.all()
 
   title = "Following"
 
@@ -72,13 +76,13 @@ def posts(request):
     if form.is_valid():
       post = Post(profile = request.user.profile, title = request.POST['title'], image = request.FILES['image'])
       post.save()
-      return redirect(reverse('profile', kwargs={'username':request.user.username}))
+      return redirect('profile', kwargs={'username':request.user.username})
   else:
     form = PostForm()
   return render(request, 'post_picture.html', {"form": form})
 
-def post(request, id):
-    post = Post.objects.get(id=id)
+def post(request, pk):
+    post = Post.objects.get(pk=pk)
     try:
         like = Like.objects.get(post=post, user=request.user)
         liked = 1
@@ -88,3 +92,98 @@ def post(request, id):
 
     return render(request, 'post.html', {"post": post})
 
+
+def explore(request):
+  random_posts = Post.objects.all().order_by('?')[:40]
+
+  
+  return render(request, 'explore.html', {"posts":random_posts})
+
+def likes(request, pk):
+  post = Post.objects.get(pk=pk)
+  profiles = Like.objects.filter(post=post)
+
+  title = 'Likes'
+  return render(request, 'follow_list.html', {"title": title, "profiles":profiles})
+
+
+@ajax_request
+@login_required
+def add_like(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
+
+@ajax_request
+@login_required
+def add_comment(request):
+    comment_text = request.POST.get('comment_text')
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    commenter_info = {}
+
+    try:
+        comment = Comment(comment=comment_text, user=request.user, post=post)
+        comment.save()
+
+        username = request.user.username
+        profile_url = reverse('profile', kwargs={'username': request.user})
+
+        commenter_info = {
+            'username': username,
+            'profile_url': profile_url,
+            'comment_text': comment_text
+        }
+
+
+        result = 1
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk,
+        'commenter_info': commenter_info
+    }
+
+@ajax_request
+@login_required
+def follow_toggle(request):
+    user_profile = Profile.objects.get(user=request.user)
+    follow_profile_pk = request.POST.get('follow_profile_pk')
+    follow_profile = Profile.objects.get(pk=follow_profile_pk)
+
+    try:
+        if user_profile != follow_profile:
+            if request.POST.get('type') == 'follow':
+                user_profile.following.add(follow_profile)
+                follow_profile.followers.add(user_profile)
+            elif request.POST.get('type') == 'unfollow':
+                user_profile.following.remove(follow_profile)
+                follow_profile.followers.remove(user_profile)
+            user_profile.save()
+            result = 1
+        else:
+            result = 0
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'type': request.POST.get('type'),
+        'follow_profile_pk': follow_profile_pk
+    }
